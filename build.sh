cd8c5d3a3318a74eb2f1016febb5f419f85abc0f
Narciso Benigno <narciso.benigno@gmail.com>
2014-10-18 19:14:51 -0300

    Add tmux support

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
+
+function confln {
+  ln -si "${sourceDir}/$1" ~/.$1
+}
+
+sourceDir="${HOME}/Dropbox/dev-box"
 
 echo 'Set up vim...'
-sourceDir="${HOME}/Dropbox/dev-box"
-ln -si "${sourceDir}/.vim" ~/.vim
+confln 'vim'
 ln -i ~/.vim/vimrc ~/.vimrc
 vim +PluginInstall +qall
 echo 'Vim has been set up!'
+
+set -x
+echo 'Set up tmux...'
+confln 'tmux'
+ln -i ~/.tmux/tmux.conf ~/.tmux.conf
+echo 'tmux has been set up!'
