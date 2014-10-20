d881af19141320728a73750875c97dc9cd88a493
Narciso Benigno <narciso.benigno@locaweb.com.br>
2014-10-20 12:08:28 -0200

    Change to use HOME var.

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ sourceDir="${HOME}/Dropbox/dev-box"
 
 echo 'Set up vim...'
 confln 'vim'
-ln -i ~/.vim/vimrc ~/.vimrc
+ln -i $HOME/.vim/vimrc $HOME/.vimrc
 vim +PluginInstall +qall
 echo 'Vim has been set up!'
 
 echo 'Set up tmux...'
 confln 'tmux'
-ln -i ~/.tmux/tmux.conf ~/.tmux.conf
+ln -i $HOME/.tmux/tmux.conf $HOME/.tmux.conf
 echo 'tmux has been set up!'
