9a259492535fd787c79818a45277ff8a458bb615
Narciso Benigno <narciso.benigno@gmail.com>
2014-10-23 23:17:19 -0200

    Add haskell support and collor scheme.

--- confs/vim.sh
+++ confs/vim.sh
@@ -7,7 +7,4 @@ installMissing 'macvim' '--env-std --override-system-vim'
 confln 'vim'
 ln -i $HOME/.vim/vimrc $HOME/.vimrc
 vim +PluginInstall +qall
-if [ -f "${sourceDir}/confs/vimext/haskell.sh" ]; then
-  $sourceDir/confs/vimext/haskell.sh
-fi
 echo 'Vim has been set up!'
