277f293e0314686b8a6abc426cf88d599cdd8f6d
Narciso Benigno <narciso.benigno@gmail.com>
2014-10-23 23:50:37 -0200

    Improve haskell support

--- confs/vim.sh
+++ confs/vim.sh
@@ -3,8 +3,12 @@
 echo 'Set up vim...'
 
 installMissing 'macvim' '--env-std --override-system-vim'
+installMissing 'cabal-install'
 
 confln 'vim'
 ln -i $HOME/.vim/vimrc $HOME/.vimrc
 vim +PluginInstall +qall
+
+make --directory="${HOME}/.vim/bundle/vimproc.vim/"
+
 echo 'Vim has been set up!'
