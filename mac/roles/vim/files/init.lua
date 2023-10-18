1789f6ff96cad079333ea1f439fa1b1f5415df06
Narciso Benigno <narciso.benigno@gmail.com>
2023-10-18 23:13:33 +1100

    configure plugin manager

--- mac/roles/vim/files/init.lua
+++ mac/roles/vim/files/init.lua
@@ -1,7 +1,27 @@
-vim.cmd "colorscheme delek"
+require "plugins"
+
+local set = vim.opt
+
+vim.cmd "set nocompatible"
+
+vim.cmd "syntax enable"
+vim.cmd "filetype plugin on"
+
+vim.cmd 'let mapleader=","'
+vim.cmd.colorscheme("delek")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.cmd "set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<"
 vim.cmd "set list"
+
+-- tabsize
+set.tabstop = 4
+set.shiftwidth = 4
+set.expandtab = false
+
+-- Buffers
+vim.keymap.set("n", "<leader>b", vim.cmd.Buffers)
+vim.keymap.set("n", "<leader>f", vim.cmd.Files)
+
